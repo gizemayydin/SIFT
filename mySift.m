addf3e6f5243a66e1bf203db3a3bb5ec32ac3186
gizemayydin <gizemaydin96@gmail.com>
2019-01-03 22:41:21 +0300

    comments

--- mySift.m
+++ mySift.m
@@ -1,19 +1,26 @@
-%clear all; clc; close all;
 function [interest_points,descriptors] = mySift(imgpath)
-
+%%%%%%%%%%%%%%%
+% This function outputs the descriptors (feature vectors) obtained from the given image
+% and a matrix where there is a row for each point that the feature vector
+% describes, containing its location, scale, orientation information.
+%%%%%%%%%%%%%%%%%
 
 %% read the image
 img = imread(imgpath);
+
+%convert to grayscale if RGB
 if size(img,3) ~= 1
     img = rgb2gray(img);
 end
+
+%convert the intensity values from integer to double for calculation
 img = double(img)/255;
 
 %% expand input image by a factor of two using bilinear interpolation
 img = imresize(img,2,'bilinear');
 img = imgaussfilt(img,1);
 
-%% scales
+%% scales that will be used in the scale-space
 scales = [0 sqrt(2) 2 2*sqrt(2) 4;
     sqrt(2) 2 2*sqrt(2) 4 4*sqrt(2);
     2 2*sqrt(2) 4 4*sqrt(2) 8;
@@ -24,41 +31,64 @@ img2 = imresize(img,0.5,'bilinear');
 img3 = imresize(img2,0.5,'bilinear');
 img4 = imresize(img3,0.5,'bilinear');
 
-%% image pyramid
-imgPyramid = {img, imgaussfilt(img,scales(1,2)), imgaussfilt(img,scales(1,3)),imgaussfilt(img,scales(1,4)),imgaussfilt(img,scales(1,5));
-    imgaussfilt(img2,scales(2,1)), imgaussfilt(img2,scales(2,2)), imgaussfilt(img2,scales(2,3)),imgaussfilt(img2,scales(2,4)),imgaussfilt(img2,scales(2,5));
-    imgaussfilt(img3,scales(3,1)),imgaussfilt(img3,scales(3,2)),imgaussfilt(img3,scales(3,3)),imgaussfilt(img3,scales(3,4)),imgaussfilt(img3,scales(3,5));
-    imgaussfilt(img4,scales(4,1)),imgaussfilt(img4,scales(4,2)),imgaussfilt(img4,scales(4,3)),imgaussfilt(img4,scales(4,4)),imgaussfilt(img4,scales(4,5))};
+%% image pyramid (scale space)
+imgPyramid = {img, imgaussfilt(img,scales(1,2)), imgaussfilt(img,scales(1,3))...
+    ,imgaussfilt(img,scales(1,4)),imgaussfilt(img,scales(1,5));
+    imgaussfilt(img2,scales(2,1)), imgaussfilt(img2,scales(2,2)),...
+    imgaussfilt(img2,scales(2,3)),imgaussfilt(img2,scales(2,4)),imgaussfilt(img2,scales(2,5));
+    imgaussfilt(img3,scales(3,1)),imgaussfilt(img3,scales(3,2)),imgaussfilt(img3,scales(3,3))...
+    ,imgaussfilt(img3,scales(3,4)),imgaussfilt(img3,scales(3,5));
+    imgaussfilt(img4,scales(4,1)),imgaussfilt(img4,scales(4,2)),...
+    imgaussfilt(img4,scales(4,3)),imgaussfilt(img4,scales(4,4)),imgaussfilt(img4,scales(4,5))};
 
 
 %% DOG pyramid
-dogPyramid = {imgPyramid{1,2}-imgPyramid{1,1}, imgPyramid{1,3}-imgPyramid{1,2}, imgPyramid{1,4}-imgPyramid{1,3},imgPyramid{1,5}-imgPyramid{1,4};
-    imgPyramid{2,2}-imgPyramid{2,1}, imgPyramid{2,3}-imgPyramid{2,2}, imgPyramid{2,4}-imgPyramid{2,3},imgPyramid{2,5}-imgPyramid{2,4};
-    imgPyramid{3,2}-imgPyramid{3,1}, imgPyramid{3,3}-imgPyramid{3,2}, imgPyramid{3,4}-imgPyramid{3,3},imgPyramid{3,5}-imgPyramid{3,4};
-    imgPyramid{4,2}-imgPyramid{4,1}, imgPyramid{4,3}-imgPyramid{4,2}, imgPyramid{4,4}-imgPyramid{4,3},imgPyramid{4,5}-imgPyramid{4,4}};
+dogPyramid = {imgPyramid{1,2}-imgPyramid{1,1}, imgPyramid{1,3}-imgPyramid{1,2}, ...
+    imgPyramid{1,4}-imgPyramid{1,3},imgPyramid{1,5}-imgPyramid{1,4};
+    imgPyramid{2,2}-imgPyramid{2,1}, imgPyramid{2,3}-imgPyramid{2,2},...
+    imgPyramid{2,4}-imgPyramid{2,3},imgPyramid{2,5}-imgPyramid{2,4};
+    imgPyramid{3,2}-imgPyramid{3,1}, imgPyramid{3,3}-imgPyramid{3,2},...
+    imgPyramid{3,4}-imgPyramid{3,3},imgPyramid{3,5}-imgPyramid{3,4};
+    imgPyramid{4,2}-imgPyramid{4,1}, imgPyramid{4,3}-imgPyramid{4,2},...
+    imgPyramid{4,4}-imgPyramid{4,3},imgPyramid{4,5}-imgPyramid{4,4}};
 
-%% local max and min
+%% Finding local maxima and minima
+
+%The columns of these arrays will represent:
 %x,y,value,octave no,column in octav, scale
 localMax = [];
 localMin = [];
 
-for i=1:1:4 %for octav
-    for j=2:1:3 %col in octav
+for i=1:1:4 %for rows in octav
+    for j=2:1:3 %for columns in octav
+        
+        %First, the image and its up and down neighbors are obtained from
+        %the DOG pyramid
         myImg = double(dogPyramid{i,j});
         upImg = double(dogPyramid{i,j-1});
         downImg = double(dogPyramid{i,j+1});
         rows = size(myImg,1);
         cols = size(myImg,2);
+        
         for k=2:1:rows-1 %row of image
-            for l=2:1:cols-1 %col of image
+            for l=2:1:cols-1 %column of image
                 
-                %check neighbors
+                %Extract the intensity value of the pixel (k,l) and its
+                %26-neighbors's
                 value = myImg(k,l);
-                neighbors = [myImg(k-1,l-1) myImg(k-1,l) myImg(k-1,l+1) myImg(k,l-1) myImg(k,l+1) myImg(k+1,l-1) myImg(k+1,l) myImg(k+1,l+1) ...
-                    upImg(k-1,l-1) upImg(k-1,l) upImg(k-1,l+1) upImg(k,l-1) upImg(k,l) upImg(k,l+1) upImg(k+1,l-1) upImg(k+1,l) upImg(k+1,l+1) ...
-                    downImg(k-1,l-1) downImg(k-1,l) downImg(k-1,l+1) downImg(k,l-1) downImg(k,l) downImg(k,l+1) downImg(k+1,l-1) downImg(k+1,l) downImg(k+1,l+1)];
+                neighbors = [myImg(k-1,l-1) myImg(k-1,l) myImg(k-1,l+1) myImg(k,l-1) ...
+                    myImg(k,l+1) myImg(k+1,l-1) myImg(k+1,l) myImg(k+1,l+1) ...
+                    upImg(k-1,l-1) upImg(k-1,l) upImg(k-1,l+1) upImg(k,l-1) upImg(k,l)...
+                    upImg(k,l+1) upImg(k+1,l-1) upImg(k+1,l) upImg(k+1,l+1) ...
+                    downImg(k-1,l-1) downImg(k-1,l) downImg(k-1,l+1) downImg(k,l-1) ...
+                    downImg(k,l) downImg(k,l+1) downImg(k+1,l-1) downImg(k+1,l) downImg(k+1,l+1)];
+                
+                %boolean values indicating whether a pixel is a local
+                %extrema candidate
                 ismax = 1;
                 ismin = 1;
+                
+                %check whether it is a local extrema
                 for m=1:1:26
                     if ismax ==1 || ismin == 1
                         if value <= neighbors(m)
@@ -72,11 +102,11 @@ for i=1:1:4 %for octav
                     end
                 end
                 
-                %it is a max
+                %if it is a maximum
                 if ismax == 1 && ismin == 0
                     localMax = [localMax; k l value i j scales(i,j)];
                     
-                    %it is a min
+                    %if it is a minimum
                 elseif ismin == 1 && ismax ==0
                     localMin = [localMin; k l value i j scales(i,j)];
                 end
@@ -85,15 +115,25 @@ for i=1:1:4 %for octav
     end
 end
 
+%Despite its name, now localMax stores all the extrema
 localMax = [localMax;localMin];
 
 %% Taylor Expansion for subpixel maxima/minima
 [maxRow, maxCol] = size(localMax);
+
+%This new array will store the new interest points after some thresholding
+%and Taylor expansion.
 newLocalMax = [];
+
+%Iterates over each interest point
 for a=1:maxRow
-    %Hessian = zeros(3,3);
+    
+    %pointmax is the interest point
     pointmax = localMax(a,:);
     pointmax = double(pointmax);
+    
+    %calculate the derivatives to form the Hessian matrix using the DOG
+    %pyramid
     image = dogPyramid{pointmax(4),pointmax(5)}(pointmax(1),pointmax(2));
     
     Dx = (dogPyramid{pointmax(4),pointmax(5)}(pointmax(1),pointmax(2)+1)...
@@ -135,18 +175,22 @@ for a=1:maxRow
     Deriv = double(Deriv);
     offset = -inv(Hessian)*Deriv;
     offset = double(offset);
+    
     %Eliminating edge responses
     Hessian2 = [Dxx Dxy; Dxy Dyy];
     edgeResThres = ((10+1)^2)/10;
     myMetric = ((trace(Hessian2))^2)/det(Hessian2);
     
     if (myMetric < edgeResThres)
+        %Checking the offset
         if (abs(offset(1)) < 0.5 && abs(offset(2)) < 0.5 && abs(offset(3)) < 0.5)
             value = double(pointmax(3)) + double((1/2)*(Deriv'*offset));
             value = double(value);
             pointmax = double(pointmax);
+            %Eliminating low contrast points
             if(abs(value) > 0.03)
-                newLocalMax = [newLocalMax; pointmax(1)+offset(1) pointmax(2)+offset(2) pointmax(3) pointmax(4) pointmax(5) pointmax(6)+offset(3)];
+                newLocalMax = [newLocalMax; pointmax(1)+offset(1) pointmax(2)+offset(2)...
+                    pointmax(3) pointmax(4) pointmax(5) pointmax(6)+offset(3)];
             end
         end
     end
@@ -158,16 +202,12 @@ end
 %the eliminated keypoints are stored in: newLocalMax
 
 %First, the gradient directions and magnitudes should be computed for the
-%scale space.
-
-imgPyramid = {img, imgaussfilt(img,scales(1,2)), imgaussfilt(img,scales(1,3)),imgaussfilt(img,scales(1,4)),imgaussfilt(img,scales(1,5));
-    imgaussfilt(img2,scales(2,1)), imgaussfilt(img2,scales(2,2)), imgaussfilt(img2,scales(2,3)),imgaussfilt(img2,scales(2,4)),imgaussfilt(img2,scales(2,5));
-    imgaussfilt(img3,scales(3,1)),imgaussfilt(img3,scales(3,2)),imgaussfilt(img3,scales(3,3)),imgaussfilt(img3,scales(3,4)),imgaussfilt(img3,scales(3,5));
-    imgaussfilt(img4,scales(4,1)),imgaussfilt(img4,scales(4,2)),imgaussfilt(img4,scales(4,3)),imgaussfilt(img4,scales(4,4)),imgaussfilt(img4,scales(4,5))};
-
+%scale space. These are held in gradient_magnitude and gradient_orientation
+%cells.
 gradient_magnitude = {};
 gradient_orientation = {};
 
+%For each image in the pyramid, compute the gradient
 for i=1:1:4
     for j=1:1:5
         myImg = imgPyramid{i,j};
@@ -181,8 +221,9 @@ for i=1:1:4
             for m=2:1:cols-2
                 magn = sqrt((myImg(k+1,m)-myImg(k-1,m))^2 + (myImg(k,m+1)-myImg(k,m-1))^2 );
                 angle = atan2d((myImg(k,m+1)-myImg(k,m-1)),(myImg(k+1,m)-myImg(k-1,m)));
+                %Put the angle in range (0,360) degrees
                 if angle < 0
-                    angle = angle + 180;
+                    angle = angle + 360;
                 end
                 gradientMag(k,m) = magn;
                 orientation(k,m) = angle;
@@ -197,7 +238,7 @@ end
 
 % Now, orientation assignment
 %x,y,value,octave no,column in octav, scale, binNumber(for orientation)
-interest_points = []; 
+interest_points = [];
 for i=1:1:size(newLocalMax,1)
     histogram = zeros(1,36);
     x = newLocalMax(i,1);
@@ -243,7 +284,6 @@ for i=1:1:size(newLocalMax,1)
     max_value = max(histogram);
     
     for n=1:1:36
-        closest_three = [];
         if histogram(1,n) >= (max_value*(8/10))
             interest_points = [interest_points;x y value octaveRow octaveCol sigma n];
         end
@@ -252,12 +292,19 @@ end
 
 %% Extract descriptors
 %x,y,value,octave no,column in octav, scale, binNumber(for orientation)
+
+%holds all the feature vectors where each row corresponds to one interest
+%point
 descriptors = [];
 
 for i=1:1:size(interest_points,1)
     
+    %since the gradient_magnitude is an array, the octave number is
+    %multiplied by 5 (as there are 5 images in each octave) and the row is
+    %added to find the corresponding image
     grad_mag_img = gradient_magnitude{(interest_points(i,4)-1)*5 + (interest_points(i,5))};
     grad_orient_img = gradient_orientation{(interest_points(i,4)-1)*5 + (interest_points(i,5))};
+    %the images are padded
     grad_mag_img = padarray(grad_mag_img,[50 50],'both');
     grad_orient_img = padarray(grad_orient_img,[50 50],'both');
     
@@ -266,7 +313,7 @@ for i=1:1:size(interest_points,1)
     sigma = interest_points(i,6);
     keypointOrientation = interest_points(i,7);
     
-    %construct the 16x16
+    %construct the 16x16 window
     xfloor = floor(x);
     xceil = ceil(x);
     yfloor = floor(y);
@@ -281,51 +328,49 @@ for i=1:1:size(interest_points,1)
     magn_window = grad_mag_img(xfloor+50-7:xceil+50+7,yfloor+50-7:yceil+50+7);
     orient_window = grad_orient_img(xfloor+50-7:xceil+50+7,yfloor+50-7:yceil+50+7);
     
+    %for each 4x4 block in the 16x16 window
     descriptor = [];
-    %for each 4x4 block
-    histAngles = [];
     for j=1:4:13
         for k=1:4:13
             histogram = [0 0 0 0 0 0 0 0];
             magn_block = magn_window(j:j+3,k:k+3);
             orient_block = orient_window(j:j+3,k:k+3);
-            kernel = fspecial('gaussian', 4, sigma);
             for m=1:1:4
                 for n=1:1:4
                     angle = orient_block(m,n);
+                    %from the previous bin number, extract the average
+                    %degree of the bin and subtract it from the angle to
+                    %find the relative angle with respect to the dominant orientation
                     angle = mod(angle - ((keypointOrientation*10)-5),360);
                     magnitude = magn_block(m,n);
+                    kernel = fspecial('gaussian', 4, sigma);
                     coeff = kernel(m,n);
                     binNumber = floor(angle/45)+1;
                     if angle ~= 360
-                        histogram(binNumber) = histogram(binNumber) + (magnitude*coeff); %%
+                        histogram(binNumber) = histogram(binNumber) + (magnitude*coeff);
                     else
                         histogram(1) = histogram(1) + (magnitude*coeff);
                     end
                 end
             end
             descriptor = [descriptor histogram];
-          %  histAngles = [histAngles 22.5 2*45-22.5 3*45-22.5 4*45-22.5 5*45-22.5 6*45-22.5 7*45-22.5 8*45-22.5];
         end
     end
     
-   % descriptor = mod(descriptor - (10*interest_points(i,6)-5),360);
-   % descriptor = mod(descriptor - angle,360);
-   % disp(descriptor - interest_points(n));
-    %disp(descriptor);
-  % newAngles = mod( histAngles - (interest_points(i,7)*36-5),360);
-  % newHistBins =  floor(newAngles/45)+1;
-  % disp(newHistBins);
-    descriptor = descriptor/(norm(descriptor));  
+    %normalize the feature vector, force the values greater than 0.2 to be
+    %0.2 and normalize again.
+    descriptor = descriptor/(norm(descriptor));
     for b = 1:128
         if descriptor(b)>0.2
             descriptor(b) = 0.2;
         end
     end
-    descriptor = descriptor/(norm(descriptor));  
+    descriptor = descriptor/(norm(descriptor));
+    
+    %Add the descriptor as a new row
     descriptors = [descriptors; descriptor];
 end
-%disp(descriptor);
+
 end
 
 
--- showPyramid.m
+++ showPyramid.m
@@ -1,10 +1,12 @@
 function showPyramid(numOctaves, numElements, imgPyramid)
+%Takes an image pyramid and its dimensions as an input and shows each octave
+%in a seperate figure
 plotDim = ceil(sqrt(numElements));
 for i=1:1:numOctaves
     figure;
     for j=1:1:numElements
-    subplot(plotDim,plotDim,j);
-    imshow(imgPyramid{i,j});
+        subplot(plotDim,plotDim,j);
+        imshow(imgPyramid{i,j});
     end
 end
 end
